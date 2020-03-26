8772c93d9970a2f51aed283aa9a5c2c524c8e301
Greg Brimble <developer@gregbrimble.com>
2020-03-26 16:21:27 +0000

    Add comments

--- src/pages/comments.js
+++ src/pages/comments.js
@@ -1,10 +1,24 @@
 import React from "react"
+import ReactUtterances from "react-utterances"
 import SEO from "../components/seo"
+
+const REPO = "louis-threedots/website"
 
 const Comments = () => (
   <>
     <SEO title="Comments" />
-    Comments Page
+
+    <div class="bg-white">
+      <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 md:py-16 lg:px-8 lg:py-20">
+        <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+          Got a question?
+          <br />
+          <span class="text-indigo-600">Ask away.</span>
+        </h2>
+      </div>
+    </div>
+
+    <ReactUtterances repo={REPO} type={"issue-number"} issueNumber={1} />
   </>
 )
 
