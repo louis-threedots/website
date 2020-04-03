93e9a96889d933ccdbd6d74379c1d9eabf44d49d
Greg Brimble <developer@gregbrimble.com>
2020-04-03 19:42:09 +0100

    Add TTS

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react"
+import { useSpeechRecognition, useSpeechSynthesis } from "react-speech-kit"
 import LoadingModal from "../components/loadingModal"
 import usePyodide from "./usePyodide"
 
@@ -8,7 +9,26 @@ const Demo = () => {
   }
 
   const { loading, attachGlobal, runPython } = usePyodide(callback)
+  const { speak, voices } = useSpeechSynthesis()
+  const [voice, setVoice] = useState()
+  const { listen, listening, stop } = useSpeechRecognition({
+    onResult: result => console.log(result),
+  })
   const [content, setContent] = useState("Loading...")
+
+  useEffect(() => {
+    if (voices.length > 0) {
+      const defaultVoice = voices.find(voice => voice.default) || voices[0]
+      setVoice(defaultVoice)
+    }
+  }, [voices])
+
+  useEffect(() => {
+    speak({
+      text: `Hi there! I'm louis, the brailliant assistant! Pleased to meet you.`,
+      voice,
+    })
+  }, [voice])
 
   useEffect(() => {
     if (!loading) {
@@ -40,6 +60,7 @@ const Demo = () => {
                     Cells (3)
                   </h3>
                 </div>
+
                 <div className="ml-4 mt-2 flex-shrink-0">
                   <span className="inline-flex rounded-md shadow-sm">
                     <button
@@ -56,7 +77,37 @@ const Demo = () => {
               Content from Python: <br />
               {content}
             </div>
-            <div className="spinner w-32 h-32"></div>
+
+            <div className="px-4 py-3 bg-gray-50 text-right sm:px-6 flex flex-row-reverse">
+              <div className="text-left">
+                <label
+                  for="voice"
+                  className="block text-sm font-medium leading-5 text-gray-700"
+                >
+                  Voice
+                </label>
+                <select
+                  id="voice"
+                  className="mt-1 block form-select w-full py-2 px-3 py-0 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  onChange={event =>
+                    setVoice(
+                      voices.find(
+                        voice => voice.voiceURI === event.target.value
+                      )
+                    )
+                  }
+                >
+                  {Array.isArray(voices) &&
+                    voices
+                      .filter(voice => voice.lang.startsWith("en-"))
+                      .map(voice => (
+                        <option key={voice.voiceURI} value={voice.voiceURI}>
+                          {voice.name} ({voice.lang})
+                        </option>
+                      ))}
+                </select>
+              </div>
+            </div>
           </div>
         </div>
       </div>
