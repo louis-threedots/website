3526305b7897e0ed6bd37f07221c221f6915202b
nina-werklaptop <n.becx@flexpulse.com>
2020-04-06 11:43:32 +0200

    start

--- src/components/documentation/troubleshootingGuide.js
+++ src/components/documentation/troubleshootingGuide.js
@@ -3,16 +3,19 @@ import React from "react"
 const TroubleshootingGuide = () => (
   <div>
     <h2 id="troubleshooting-guide">Troubleshooting Guide</h2>
-    <p>TODO</p>
+    <p>
+      The troubleshooting guide is classified into four sections:
+      <strong>Main Controller</strong>, <strong>Cells</strong>, <strong>Apps</strong>, and <strong>FAQ</strong>.
+    </p>
     <div className="my-6 border-t border-b border-gray-300 overflow-hidden relative">
       <table className="w-full text-left">
         <thead>
           <tr>
             <th className="text-sm font-semibold text-gray-700 p-2 bg-gray-100 w-2/5">
-              Lectus a sapien metus
+              Issues
             </th>
             <th className="text-sm font-semibold text-gray-700 p-2 bg-gray-100">
-              Lectus a sapien metus
+              Solutions
             </th>
           </tr>
         </thead>
