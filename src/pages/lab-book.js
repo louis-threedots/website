b223efc5fc3b1a1d0344640b6d00d5a79761614b
Greg Brimble <developer@gregbrimble.com>
2020-04-08 15:02:15 +0100

    Add labbook

---
+++ src/pages/lab-book.js
@@ -0,0 +1,11 @@
+import React from "react"
+import SEO from "../components/seo"
+
+const LabBook = () => (
+  <>
+    <SEO title="Lab Book" />
+    TODO: An archive/gallery/history/diary/lab-book thing
+  </>
+)
+
+export default LabBook
