315eef0be50157d5fc30835d9a555228ed3af990
Greg Brimble <developer@gregbrimble.com>
2020-04-09 00:31:10 +0100

    Start How it Works styles

--- src/components/firmwareSimulator.js
+++ src/components/firmwareSimulator.js
@@ -1,29 +1,33 @@
 import React, { useState } from "react"
-import { graphql, useStaticQuery } from "gatsby"
-import Img from "gatsby-image"
 
 const FirmwareSimulator = ({ steps, title }) => {
   const [step, setStep] = useState(0)
   const hasNextStep = step < steps.length - 1
   const hasPreviousStep = step > 0
-  const buttonCSS = " bg-gray-50 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 "
-  const disabledCSS = "opacity-50 cursor-not-allowed"
-  const disabledPreviousCSS = hasPreviousStep?"":disabledCSS
-  const disabledNextCSS = hasNextStep?"":disabledCSS
 
   const { image, description } = steps[step]
 
   return (
     <div>
       <h4>{title}</h4>
-      <p>Step {step+1} of {steps.length}</p>
+      <p>
+        Step {step + 1} of {steps.length}
+      </p>
       <div>{image}</div>
       <div>{description}</div>
-      <div class="inline-flex">
-        <button disabled={!hasPreviousStep} className={"rounded-l" + buttonCSS + disabledPreviousCSS} onClick={() => setStep(step - 1)}>
+      <div className="inline-flex">
+        <button
+          disabled={!hasPreviousStep}
+          className="rounded-l bg-gray-50 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 disabled:opacity-50 disabled:cursor-not-allowed"
+          onClick={() => setStep(step - 1)}
+        >
           Previous
         </button>
-        <button disabled={!hasNextStep} className={"rounded-r" + buttonCSS + disabledNextCSS}  onClick={() => setStep(step + 1)}>
+        <button
+          disabled={!hasNextStep}
+          className="rounded-r bg-gray-50 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 disabled:opacity-50 disabled:cursor-not-allowed"
+          onClick={() => setStep(step + 1)}
+        >
           Next
         </button>
       </div>
