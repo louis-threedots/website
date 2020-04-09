458c79c63599b1dbe75ec9a32e34e19e64bee995
nina-werklaptop <n.becx@flexpulse.com>
2020-04-09 13:35:40 +0200

    another bugfix :P

--- src/pages/comments.js
+++ src/pages/comments.js
@@ -4,12 +4,12 @@ import SEO from "../components/seo"
 const Comments = () => (
   <>
     <SEO title="Comments" />
-    <div class="bg-white">
-      <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 md:py-16 lg:px-8 lg:py-20">
-        <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+    <div className="bg-white">
+      <div className="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 md:py-16 lg:px-8 lg:py-20">
+        <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
           Got a question?
           <br />
-          <span class="text-indigo-600">Ask away.</span>
+          <span className="text-indigo-600">Ask away.</span>
         </h2>
         The official University of Edinburgh comment page can be found <a href="https://sdp2020.gitlab.io/posts/group10/" className="font-medium text-gray-500 underline">here</a>.
       </div>
