d60567890ad7ca9fabf3d090200ef3b83e2450c2
Ragnor Comerford <hello@ragnor.co>
2020-04-09 17:37:47 +0400

    Improve design of hardware simulator.

--- src/components/firmwareSimulator.js
+++ src/components/firmwareSimulator.js
@@ -9,13 +9,15 @@ const FirmwareSimulator = ({ steps, title }) => {
   const { image, description } = steps[step]
 
   return (
-    <div>
-      <h4>{title}</h4>
-      <p>
+    <div className="rounded-lg bg-indigo-50 px-6 py-8 sm:p-10 my-10 flex flex-col items-center">
+      <h3 className="text-xl leading-8 font-extrabold text-gray-900 sm:text-2xl sm:leading-9">
+        {title}
+      </h3>
+      <p className="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">
         Step {step + 1} of {steps.length}
       </p>
-      <div>{image}</div>
-      <div>{description}</div>
+      <div className="w-1/2 my-10">{image}</div>
+      <div className="text-base text-gray-700 my-5">{description}</div>
       <div className="inline-flex">
         <button
           disabled={!hasPreviousStep}
