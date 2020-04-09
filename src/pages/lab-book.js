6f20b78368b2c54ba75a1770943ee28cabc0adfe
Greg Brimble <developer@gregbrimble.com>
2020-04-09 11:16:44 +0100

    Lab book styles

--- src/pages/lab-book.js
+++ src/pages/lab-book.js
@@ -1,23 +1,22 @@
 import React from "react"
+import ImageGallery from "react-image-gallery"
 import SEO from "../components/seo"
-import ImageGallery from 'react-image-gallery';
-// import "~react-image-gallery/styles/scss/image-gallery.scss";
-// import "~react-image-gallery/styles/css/image-gallery.css"
- 
+import "./lab-book.css"
+
 const images = [
   {
-    original: 'https://picsum.photos/id/1018/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1018/250/150/',
+    original: "https://picsum.photos/id/1018/1000/600/",
+    thumbnail: "https://picsum.photos/id/1018/250/150/",
   },
   {
-    original: 'https://picsum.photos/id/1015/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1015/250/150/',
+    original: "https://picsum.photos/id/1015/1000/600/",
+    thumbnail: "https://picsum.photos/id/1015/250/150/",
   },
   {
-    original: 'https://picsum.photos/id/1019/1000/600/',
-    thumbnail: 'https://picsum.photos/id/1019/250/150/',
+    original: "https://picsum.photos/id/1019/1000/600/",
+    thumbnail: "https://picsum.photos/id/1019/250/150/",
   },
-];
+]
 
 const LabBook = () => (
   <>
